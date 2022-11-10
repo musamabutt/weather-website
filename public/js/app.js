00b18dfbb0af0956d9bb98dc8e39dbf653ad9658
Muhammad Usamaa <muhammad.usamaa@RT-LT-531.rolustech.net>
2022-11-10 12:53:48 +0500

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+console.log("Client side JS is loaded!");
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const message1 = document.querySelector('#message1');
+const message2 = document.querySelector('#message2');
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value;
+    message1.textContent = 'Loading...';
+    message2.textContent = '';
+    fetch('http://localhost:3000/Address?address='+location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            message1.textContent = data.error;
+        } else {
+            message1.textContent = data.location;
+            message2.textContent = data.forecast;
+        }
+    });
+});
+});
